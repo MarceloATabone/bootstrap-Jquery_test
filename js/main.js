7d51108bdfa463f9e8f4ccf8d7d379c40c1b32fe
Marcelo A Tabone <marceloatabone@hotmail.com>
2020-03-09 13:06:55 -0300

    Creating a website base
    Creating the basis of a used website:
       - Bootstrap and JQuery

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
+$(function(){
+    $(".header").load("views/includes/header.html"); 
+});
+
+$(function(){
+    $(".footer").load("views/includes/footer.html"); 
+});
+
+
+
+
+
